cd608ee7a0b6b6d2f604182131fd8730b9cdcdb0
Roldan <jesus.roldan82@hotmail.com>
2025-10-14 16:37:44 +0200

    Ajout des migrations pour les tables users et clients

---
+++ database/migrations/2025_10_14_133947_create_clients_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string("name", 100);
+            $table->string("email", 150)->unique();
+            $table->string("phone", 20)->nullable();
+            $table->string("address", 200)->nullable();
+            $table->datetime("created_at")->nullable();
+            $table->datetime("update_at")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clients');
+    }
+};
